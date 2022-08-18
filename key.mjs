16fab51a22bde279dc64724287dfe54dbedc5af4
Azl23635 <rmitanzel@gmail.com>
2022-08-19 02:14:14 +1000

    minor: comments

--- key.mjs
+++ key.mjs
@@ -1,4 +1,5 @@
 export default class Key {
+  //Function shifts the bit by one (to left)
   static #rotateKey = (key, shiftBy = 1) => {
     let rotateOut = '';
     for (let i = 0; i < key.length; i++) {
@@ -12,6 +13,7 @@ export default class Key {
   static #one_dropThenPermute = (key64) => {
     let left = '';
     let right = '';
+    //Lookup table for first permutation
     let pcLookup = [
       [
         57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2,
@@ -22,6 +24,7 @@ export default class Key {
         61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4,
       ],
     ];
+    //Order bits according to lookup table
     for (let i = 0; i < pcLookup.length; i++) {
       for (let j = 0; j < pcLookup[i].length; j++) {
         i === 0
@@ -29,37 +32,42 @@ export default class Key {
           : (right += key64[pcLookup[i][j] - 1]);
       }
     }
+    //Return the left and right sides
     return [left, right];
   };
 
   static #two_bitRotate = (keyLeftRight56, round) => {
     let keyLeft = keyLeftRight56[0];
     let keyRight = keyLeftRight56[1];
+    //Lookup bitshift for every round
     const bitShiftLookup = [
       1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1,
     ];
     const output = [];
+    //Adds up the num of bit shift required for current round
     const bitShiftForRound = bitShiftLookup
       .slice(0, round)
       .reduce((total, curr) => total + curr, 0);
-
+    //Bit shift by required amount for both left/right sides
     for (let i = 0; i < bitShiftForRound; i++) {
       keyLeft = this.#rotateKey(keyLeft);
       keyRight = this.#rotateKey(keyRight);
     }
+    //Concatenate the keys together to return the bit shifted key
     output.push(keyLeft + keyRight);
     return output;
   };
 
   static #three_permutationTwo = (keyLeftRightRotated) => {
     let pcTwoOutput48 = '';
+    //Lookup table for permutation two
     const pcTwoLookup = [
       14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26,
       8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45,
       33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32,
     ];
     const rotatedKey = keyLeftRightRotated[0];
-
+    //Do the permutation, return result
     for (let i = 0; i < pcTwoLookup.length; i++) {
       pcTwoOutput48 += rotatedKey[pcTwoLookup[i] - 1];
     }
